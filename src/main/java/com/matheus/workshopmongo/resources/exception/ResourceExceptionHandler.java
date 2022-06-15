5445610efa6f29bfe0cb4589322ee23c616e9695
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-15 11:15:41 -0300

    findById implementation with handling exception

---
+++ src/main/java/com/matheus/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.matheus.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.matheus.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request){		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(),status.value(),"User not found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/com/matheus/workshopmongo/services/UserService.java
+++ src/main/java/com/matheus/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheus.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.workshopmongo.domain.User;
 import com.matheus.workshopmongo.repositories.UserRepository;
+import com.matheus.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,15 @@ public class UserService {
 		return repo.findAll();
 
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() ->  new ObjectNotFoundException("Object not found."));
+	}
+	
+	
+	
+	
+	
 
 }
