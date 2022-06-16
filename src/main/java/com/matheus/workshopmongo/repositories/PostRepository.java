ca28d1bae0667575bc974e8ee9085c00d7cba8c6
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-16 15:06:07 -0300

    Simple query with @Query

--- src/main/java/com/matheus/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/matheus/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.matheus.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.matheus.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.matheus.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	public List<Post> customFindByTitle(String text);
 	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 
